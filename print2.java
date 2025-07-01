0c21ce72315ae8ddb9533515083f462607bb2d18
6704062611328 <s6704062611328@email.kmutnb.ac.th>
2025-07-02 01:44:24 +0700

    Create print2.java

---
+++ print2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in); 
+        String show = input.nextLine();        
+        System.out.println(show);         
+    }
+}
