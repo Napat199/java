311f34aef9e634b79d76ea999d0ed1f95c8d99e8
6704062611328 <s6704062611328@email.kmutnb.ac.th>
2025-07-02 01:44:01 +0700

    Create print.java

---
+++ print.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class Infoo {
+    public static void main(String[] args){
+        String Inform = JOptionPane.showInputDialog("ssss");
+        JOptionPane.showMessageDialog(null, "สวัสดี " + Inform + "!");
+    }
+}
